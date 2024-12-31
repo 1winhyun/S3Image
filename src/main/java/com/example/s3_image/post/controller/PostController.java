4341b6a2c24fc131f7a93bab770988e9a988bf90
1winhyun <jimking1@naver.com>
2024-12-31 23:48:45 +0900

    refactor: 앤드포인트 수정

--- src/main/java/com/example/s3_image/post/controller/PostController.java
+++ src/main/java/com/example/s3_image/post/controller/PostController.java
@@ -11,16 +11,18 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/post")
 public class PostController {
     private final PostService postService;
 
-    @PostMapping(value = "/api/post",consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity<PostResponseDTO>createPost(@RequestPart PostRequestDTO requestDTO,
                                                      @RequestPart List<MultipartFile>images) throws IOException {
         PostResponseDTO post=postService.createPost(requestDTO,images);
@@ -28,7 +30,7 @@ public class PostController {
                 .body(post);
     }
 
-    @GetMapping("/api/post")
+    @GetMapping
     public ResponseEntity<List<PostResponseDTO>>findAllPosts(){
         List<PostResponseDTO> posts=postService.findAllPosts();
         return ResponseEntity.ok()
